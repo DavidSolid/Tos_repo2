8528b4ff94f829d2a10ad28a1ea381da39cd7311
PcPortatile <cancelli1997@gmail.com>
2018-12-07 12:51:20 +0100

    conformata classe a checkstyle

--- restaurant-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
+++ restaurant-manager/src/main/java/it/unipd/tos/business/exception/RestaurantBillException.java
@@ -4,11 +4,11 @@
 package it.unipd.tos.business.exception;
 
 public class RestaurantBillException extends Exception {
-	private String error;
-	public RestaurantBillException(String error) {
-		this.error=error;
-	}
-	public String getError() {
-		return error;
-	}
+private String error;
+public RestaurantBillException(String error) {
+	this.error=error;
+}
+public String getError() {
+	return error;
+}
 }
