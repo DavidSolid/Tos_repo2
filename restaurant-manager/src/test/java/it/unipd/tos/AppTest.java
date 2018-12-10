424cc58d7535d41529f81be12e6fcb72fa829cdb
PcPortatile <cancelli1997@gmail.com>
2018-12-10 13:59:12 +0100

    ora i test sono riconosciuti

--- restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/AppTest.java
@@ -3,60 +3,66 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    //extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    //public AppTest( String testName )
+    //{
+        //super( testName );
+    //}
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    //public static Test suite()
+    //{
+        //return new TestSuite( AppTest.class );
+    //}
 
     /**
      * Rigourous Test :-)
      */
-    
+    @Test
     public void getOrderPrice_EmptyList_ExpectedZero()
     {
-    	//fail("Not implemented");
-    	assertTrue(true);
+    	fail("Not implemented");
     }
     
+    @Test
     public void getOrderPrice_ListMoreThan20Items_RestaurantBillExceptionThrown() {
     	fail("Not Implemented");
     }
     
+    @Test
     public void getOrderPrice_ListLessThan10PizzasAndSumLessThan100_ExpectedSum() {
     	fail("Not Implemented");
     }
     
+    @Test
     public void getOrderPrice_ListMoreThan10PizzasAndSumLessThan100_ExpectedSumWithoutCheapestPizza() {
     	fail("Not Implemented");
     }
     
+    @Test
     public void getOrderPrice_ListLessThan10PizzasAndSumMoreThan100_ExpectedSumWithDiscount() {
     	fail("Not Implemented");
     }
     
+    @Test
     public void getOrderPrice_ListMoreThan10PizzasAndSumMoreThan100_ExpectedSumWithoutCheapestPizzaWithDiscount() {
     	fail("Not Implemented");
     }
